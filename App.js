a89209aa63b0a5e62421134d423d40e7d1e98fb3
dekDika13 <kadekdikaramasuryadi@gmail.com>
2024-06-23 22:23:50 +0800

    feature add note

--- App.js
+++ App.js
@@ -3,17 +3,18 @@ import Home from './src/screens/home'
 import AddNote from './src/screens/addNote'
 import EditNote from './src/screens/editNote'
 
-const CurrentPageWidget = ({ currentPage, noteList, setCurrentPage }) => {
+const CurrentPageWidget = ({
+  currentPage,
+  noteList,
+  setCurrentPage,
+  addNote,
+}) => {
   switch (currentPage) {
     case 'home':
-      return (
-        <Home
-          noteList={noteList}
-          setCurrentPage={setCurrentPage}
-        />
-      )
+      return <Home noteList={noteList} setCurrentPage={setCurrentPage} />
     case 'add':
-      return <AddNote />
+      // Berikan function "addNote" ke component "AddNote"
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />
     case 'edit':
       return <EditNote />
     default:
@@ -32,14 +33,29 @@ const App = () => {
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
     },
   ])
+  const addNote = (title, desc) => {
+    const id =
+      noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1
+  
+    setNoteList([
+      ...noteList,
+      {
+        id,
+        title: title,
+        desc: desc,
+      },
+    ])
+  }
 
   return (
     <CurrentPageWidget
       currentPage={currentPage}
-      setCurrentPage={setCurrentPage}
       noteList={noteList}
+      setCurrentPage={setCurrentPage}
+      // Berikan function addNote sebagai prop
+      addNote={addNote}
     />
-  )
-}
+  );
+};
 
-export default App
+export default App;
